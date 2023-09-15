6f387455716f532f9d1e0d7bb69e93bd2bef5400
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-14 23:16:55 -0300

    id

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  constructor(private activeRouter: ActivatedRoute) { }
 
+  ngOnInit(): void {
+    this.pokemon
+  }
+
+  get pokemon() {
+    const id = this.activeRouter.snapshot.params['id']
+
+    return console.log(id)
+  }
 }
