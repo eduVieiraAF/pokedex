464ad471231c1afacad1a8abc1cc0deef6bd36d2
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-18 08:59:25 -0300

    bug fixed

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -6,33 +6,38 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  private urlPokemon: string = "https://pokeapi.co/api/v2/pokemon/"
-  private urlName: string = "https://pokeapi.co/api/v2/pokemon-species/"
-  public pokemon: any
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'; // Removi as barras finais
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'; // Removi as barras finais
+
+  public pokemon: any;
   public isLoading: boolean = false;
   public apiError: boolean = false;
 
-
-  constructor(private activeRouter: ActivatedRoute, private pokeApiService: PokeApiService) { }
+  constructor(
+    private activeRouter: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
-    this.getPokemon
+    this.getPokemon(); // invoquei a função, na aula usamos get ao invés de public
   }
 
-  public getPokemon(){
+  public getPokemon() {
     const id = this.activeRouter.snapshot.params['id'];
-    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const pokemon = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokemon}/${id}`
+    );
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
-      res => {
+      (res) => {
         this.pokemon = res;
         this.isLoading = true;
       },
-      error => {
+      (error) => {
         this.apiError = true;
       }
     );
